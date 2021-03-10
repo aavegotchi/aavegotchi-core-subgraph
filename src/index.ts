a54bc50186d2fc12db3b41678aa777f0dbfabeb0
Coderdan <coderdannn@gmail.com>
2021-03-10 21:52:21 +0800

    added marketplace handlers

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,16 @@ export {
   handleUseConsumables,
   handleSetAavegotchiName,
   handleGrantExperience,
-  handleXingyun
+  handleXingyun,
+  handleTransfer,
+  //erc1155 marketplace
+  handleERC1155ExecutedListing,
+  handleERC1155ListingCancelled,
+  handleERC1155ListingAdd,
+  handleERC1155ListingRemoved,
+  //erc721 marketplace
+  handleERC721ExecutedListing,
+  handleERC721ListingAdd,
+  handleERC721ListingCancelled,
+  handleERC721ListingRemoved,
 } from "./mappings/diamond";
