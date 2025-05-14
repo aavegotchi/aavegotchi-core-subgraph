4fefcca1f4ceac40f1f714eb7d3cf54c328316b7
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2025-05-14 09:20:43 +0200

    fix: update fake sepolia contracts

--- src/mappings/diamond.ts
+++ src/mappings/diamond.ts
@@ -113,7 +113,7 @@ import {
 } from "../utils/constants";
 import { Address, BigInt, log, Bytes } from "@graphprotocol/graph-ts";
 
-import { /*Parcel,*/ Parcel, TokenCommitment } from "../../generated/schema";
+import { Parcel, TokenCommitment } from "../../generated/schema";
 
 import { updatePermissionsFromBitmap } from "../utils/decimals";
 import * as erc7589 from "./erc-7589";
@@ -606,6 +606,8 @@ export function handleERC721ListingAdd(event: ERC721ListingAdd): void {
     listing.parcel = event.params.erc721TokenId.toString();
     let parcel = Parcel.load(event.params.erc721TokenId.toString())!;
     parcel.activeListing = event.params.listingId;
+    parcel.save();
+
     listing.fudBoost = parcel.fudBoost;
     listing.fomoBoost = parcel.fomoBoost;
     listing.alphaBoost = parcel.alphaBoost;
@@ -689,21 +691,21 @@ export function handleERC721ExecutedListing(
 
     //Parcel -- update number of times traded
 
-    // let parcel = getOrCreateParcel(
-    //   event.params.erc721TokenId,
-    //   event.params.buyer,
-    //   event.params.erc721TokenAddress
-    // );
-    // parcel.timesTraded = parcel.timesTraded.plus(BIGINT_ONE);
-    // parcel.activeListing = null;
-    // // add to historical prices
-    // let historicalPrices = parcel.historicalPrices;
-    // if (historicalPrices == null) {
-    //   historicalPrices = new Array();
-    // }
-    // historicalPrices.push(event.params.priceInWei);
-    // parcel.historicalPrices = historicalPrices;
-    // parcel.save();
+    let parcel = getOrCreateParcel(
+      event.params.erc721TokenId,
+      event.params.buyer,
+      event.params.erc721TokenAddress
+    );
+    parcel.timesTraded = parcel.timesTraded.plus(BIGINT_ONE);
+    parcel.activeListing = null;
+    // add to historical prices
+    let historicalPrices = parcel.historicalPrices;
+    if (historicalPrices == null) {
+      historicalPrices = new Array();
+    }
+    historicalPrices.push(event.params.priceInWei);
+    parcel.historicalPrices = historicalPrices;
+    parcel.save();
   }
 
   let stats = getStatisticEntity();
@@ -734,14 +736,14 @@ export function handleERC721ListingCancelled(
     gotchi.locked = false;
     gotchi.save();
   } else if (listing.category.equals(BigInt.fromI32(4))) {
-    // let parcel = getOrCreateParcel(
-    //   listing.tokenId,
-    //   listing.seller,
-    //   Address.fromString(listing.erc721TokenAddress.toHexString()),
-    //   false
-    // );
-    // parcel.activeListing = null;
-    // parcel.save();
+    let parcel = getOrCreateParcel(
+      listing.tokenId,
+      listing.seller,
+      Address.fromString(listing.erc721TokenAddress.toHexString()),
+      false
+    );
+    parcel.activeListing = null;
+    parcel.save();
   }
 
   listing.cancelled = true;
