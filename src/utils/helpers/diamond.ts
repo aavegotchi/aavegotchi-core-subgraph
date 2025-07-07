9d9908345b7c174e31e20ced92a401a86725f3d6
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2025-07-07 09:58:52 +0200

    fix: remove unused const

--- src/utils/helpers/diamond.ts
+++ src/utils/helpers/diamond.ts
@@ -1,11 +1,6 @@
 import { BigInt, Address, ethereum, store } from "@graphprotocol/graph-ts";
 import { ItemTypeOwnership } from "../../../generated/schema";
 
-// Address of the actual wearable token contract
-export const WEARABLE_TOKEN_ADDRESS = Address.fromString(
-  "0x58de9AaBCaeEC0f69883C94318810ad79Cc6a44f"
-);
-
 export function updateOwnership(
   itemTypeId: string,
   owner: Address,
